f4e15f1800fc17d415aa1dde040af6220d561ec2
Piron Evgeniy <piron.johny@gmail.com>
2021-08-06 12:27:09 +0000

    Домашнее задание номер 5

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    page: document.querySelector('[data-modal-no-scroll]'),
+  }
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.page.classList.toggle('no-scroll');
+
+  }
+})();
